5ada8dbb1ab5930e75bc36cc4aa0fdae0e0bef82
KlimKosenko <91756470+KlimKosenko@users.noreply.github.com>
2024-06-10 20:48:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello WatchTower 123131231!</h1>')
+  res.send('<h1>Hello WatchTower 123131231!</h1><br><h2>Azure test</h2>')
 })
 
 const PORT = 8080
